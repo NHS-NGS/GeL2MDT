1f8abebbca6a9c7a2cc8583f226e71c488f61cc6
Patrick Lombard <ptk.lmb55@gmail.com>
2018-01-18 15:06:33 +0000

    adding login, registration and theme

---
+++ gelweb/gel2mdt/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import *
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    """ User registration form
+    """
+    password = forms.CharField(widget=forms.PasswordInput())
+    role_choices = (('Clinician', 'Clinician'), ('CS', 'Clinical Scientist'), ('Other', 'Other Staff'))
+    role = forms.MultipleChoiceField(choices=role_choices)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'password',)
