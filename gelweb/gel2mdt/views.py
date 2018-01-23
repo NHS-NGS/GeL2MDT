23e0648618f50589f460ac6bde2815ed38f2fa0b
Patrick Lombard <patrick.lombard@gosh.nhs.uk>
2018-01-23 16:47:45 +0000

    changed staff models and working on registration

--- gelweb/gel2mdt/views.py
+++ gelweb/gel2mdt/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .forms import *
+from .models import *
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .database_utils import add_cases
+from django.contrib import messages
+from django.db import IntegrityError
 
 # Create your views here.
 def register(request):
@@ -20,12 +23,31 @@ def register(request):
             username = user_form.cleaned_data['email']
             password = user_form.cleaned_data['password']
             email = user_form.cleaned_data['email']
-            user = User(username=username, first_name=first_name, last_name=last_name, password=password,
-                        email=email, is_active=False)
-            user.save()
-            user.set_password(user.password)
-            user.save()
-            registered = True
+            role = user_form.cleaned_data['role']
+            try:
+                user = User(username=username, first_name=first_name, last_name=last_name, password=password,
+                            email=email, is_active=False)
+                user.save()
+                user.set_password(user.password)
+                user.save()
+                registered = True
+                if role == 'CS':
+                    cs = ClinicalScientist(cs_name=first_name + ' ' + last_name,
+                                           email=email)
+                    cs.save()
+                elif role == 'Clinician':
+                    clinician = Clinician(clinician_name=first_name + ' ' + last_name,
+                                          hospital='test',
+                                          email=email)
+                    clinician.save()
+                elif role == 'Other':
+                    other = OtherStaff(staff_name=first_name + ' ' + last_name,
+                                       email=email)
+                    other.save()
+            except IntegrityError as e:
+                messages.error(request, 'If you have already registered, '
+                                        'please contact Bioinformatics to activate your account')
+                return HttpResponseRedirect('/register')
 
     else:
         user_form = UserForm()
@@ -34,6 +56,6 @@ def register(request):
                   {'user_form': user_form, 'registered': registered, 'username': username})
 
 def index(request):
-    #m = add_cases.InterpretationList()
-    #print(m.all_cases_count)
+    # m = add_cases.InterpretationList()
+    # print(m.all_cases_count)
     return render(request, 'gel2mdt/index.html', {} )
--- gelweb/gelweb/settings/base.py
+++ gelweb/gelweb/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,3 +118,10 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'index'
 
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
