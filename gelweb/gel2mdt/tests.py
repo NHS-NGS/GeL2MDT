0285b5626a0ff0af1c90d6801321448e3d4be11b
theocole <theo.j.cole@gmail.com>
2018-01-23 09:13:31 +0000

    get hashes of json

--- gelweb/gel2mdt/tests.py
+++ gelweb/gel2mdt/tests.py
@@ -4,7 +4,12 @@ from .api_utils import poll_api
 from .database_utils import add_cases
 
 import re
+import os
+import json
+import hashlib
 
+
+@unittest.skip("skipping to avoid polling")
 class Poll_CIP_API_TestCase(TestCase):
     def test_returns_status_code(self):
         cip_api_poll = poll_api.PollAPI("cip_api", "interpretation-request")
@@ -37,10 +42,41 @@ class TestInterpretationList(TestCase):
 
 class TestAddCases(TestCase):
     def setUp(self):
+        """
+        Instantiate a MultipleCaseAdder for the zoo of test cases.
+        """
         self.case_update_handler = add_cases.MultipleCaseAdder(test_data=True)
 
     def test_request_id_format(self):
-        print(self.case_update_handler.list_of_cases)
+        """
+        For each test case, assert that we correctly parse the IR ID.
+        """
         for case in self.case_update_handler.list_of_cases:
-            print(case.request_id)
             assert re.match("\d+-\d+", case.request_id)
+
+    def test_hash_cases(self):
+        """
+        For each test case, assert that we reliably hash the json.
+        """
+        test_json_hashes = {}
+        for filename in os.listdir(
+            os.path.join(
+                os.getcwd(),
+                "gel2mdt/tests/test_files"
+            )
+        ):
+            # get the jsons
+            file_path = os.path.join(
+                os.getcwd(), "gel2mdt/tests/test_files/{filename}".format(
+                    filename=filename))
+            with open(file_path) as json_file:
+                json_data = json.load(json_file)
+            # get the IR ID and hash the json into a dict
+            request_id = str(json_data["interpretation_request_id"]) + \
+                "-" + str(json_data["version"])
+            test_json_hashes[request_id] = hashlib.sha512(
+                json.dumps(json_data, sort_keys=True).encode('utf-8')
+            ).hexdigest()
+
+        for case in self.case_update_handler.list_of_cases:
+            assert case.json_hash == test_json_hashes[case.request_id]
