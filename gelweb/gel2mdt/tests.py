6d890d6a6b80d87b1571137a8c81127dba8b8db6
Patrick Lombard <patrick.lombard@gosh.nhs.uk>
2018-01-24 11:33:56 +0000

    cleaned up test conflict

--- gelweb/gel2mdt/tests.py
+++ gelweb/gel2mdt/tests.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import unittest
 from django.test import TestCase
 from .api_utils import poll_api
@@ -206,24 +205,3 @@ class TestCaseOperations(object):
 
         return hash_digest
 
-class ViewTests(TestCase):
-    """
-    Testing the views
-    """
-    def login_test_user(self):
-        self.client.login(username='test@gmail.com', password='test')
-        return self
-
-    def logout_test_user(self):
-        self.client.logout()
-        return self
-
-    def test_registration(self):
-        response = self.client.post(reverse('register'), {'first_name': 'test',
-                                                          'last_name': 'test2',
-                                                          'email': 'test@gmail.com',
-                                                          'password': 'password',
-                                                          'role': 'CS',
-                                                          'hospital': 'GOSH'}, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('Thank you for registering', response.content.decode())
