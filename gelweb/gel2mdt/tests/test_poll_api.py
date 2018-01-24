8383b908719e2bf15e001fbc329f2fef1a84971d
theocole <theo.j.cole@gmail.com>
2018-01-24 11:22:12 +0000

    modified testing hierarchy to avoid conflicts

---
+++ gelweb/gel2mdt/tests/test_poll_api.py
@@ -0,0 +1,33 @@
+import unittest
+from django.test import TestCase
+from ..api_utils.poll_api import PollAPI
+
+@unittest.skip('avoid unnecessarily polling')
+class Poll_CIP_API_TestCase(TestCase):
+    def test_returns_status_code(self):
+        cip_api_poll = PollAPI("cip_api", "interpretation-request")
+        cip_api_poll.get_json_response()
+
+
+@unittest.skip('avoid unnecessarily polling')
+class TestInterpretationList(TestCase):
+    def setUp(self):
+        self.case_list_handler = add_cases.InterpretationList()
+        self.case_list = self.case_list_handler.all_cases
+        self.cases_to_poll = self.case_list_handler.cases_to_poll
+
+    def test_get_list_of_jsons(self):
+        assert isinstance(self.case_list, list)
+        for case in self.case_list:
+            assert isinstance(case, dict)
+
+    def test_only_get_raredisease(self):
+        for case in self.case_list:
+            assert case["sample_type"] == "raredisease"
+
+    def test_get_cases_of_interest(self):
+        for case in self.cases_to_poll:
+            assert case["last_status"] in [
+                "sent_to_gmcs",
+                "report_generated",
+                "report_sent"]
