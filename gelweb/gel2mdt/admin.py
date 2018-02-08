aae98d109426413bfb6bc92bf8a71a1729c97118
theocole <theo.j.cole@gmail.com>
2018-02-08 12:21:16 +0000

    registered models for viewing in admin panel

--- gelweb/gel2mdt/admin.py
+++ gelweb/gel2mdt/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+from gel2mdt.models import *
+
+
+app = apps.get_app_config('gel2mdt')
+for model_name, model in app.models.items():
+    admin.site.register(model)
