1b9128df1990327b6039812d633c23d6dcbc2ea9
Edward Stone <efgstone@outlook.com>
2018-02-12 10:09:25 +0000

    Removed int casting from labkey clinician search and changed ProbandVariant.somatic to a NullBooleanField

--- gelweb/gel2mdt/database_utils/case_handler.py
+++ gelweb/gel2mdt/database_utils/case_handler.py
@@ -219,7 +219,7 @@ class CaseAttributeManager(object):
         Create a case model to handle adding/getting the clinician for case.
         """
         # family ID used to search for clinician details in labkey
-        # family_id = int(self.case.json["family_id"])
+        # family_id = self.case.json["family_id"]
         # # load in site specific details from config file
         # config_dict = load_config.LoadConfig().load()
         # labkey_server_request = config_dict['labkey_server_request']
@@ -456,7 +456,7 @@ class CaseAttributeManager(object):
         gene_list = []
         for panel in panels:
             genes = panel["panelapp_results"]["Genes"]
-            print("Adding gene ensembl ID to list:", genes["GeneSymbol"])
+            #print("Adding gene ensembl ID to list:", genes["GeneSymbol"])
             gene_list += genes
 
         for gene in gene_list:
