fab22c60c48ba756ba82c22b350f2baa48cb4f0a
Theo Cole <theo.j.cole@gmail.com>
2018-03-07 21:48:40 +0000

    using REST framework API views

--- gelweb/gel2mdt/api/api_views.py
+++ gelweb/gel2mdt/api/api_views.py
@@ -1,16 +1,17 @@
 from gel2mdt.models import *
-from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from rest_framework.decorators import api_view
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 from gel2mdt.api.serializers import GELInterpretationReportSerializer
 
 
-@login_required
+@api_view(['GET'])
 def rare_disease_json(request):
     '''
     JSON response for all rare disease cases.
     '''
     all_gel_irs = GELInterpretationReport.objects.all()
     serializer = GELInterpretationReportSerializer(all_gel_irs, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    return Response(serializer.data)
