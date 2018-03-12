fd7130091d773fa980fde239dad849f0279f923c
Patrick Lombard <patrick.lombard@gosh.nhs.uk>
2018-03-12 15:30:11 +0000

    updating selecting transcript

--- gelweb/gel2mdt/templatetags/gel2mdt_extras.py
+++ gelweb/gel2mdt/templatetags/gel2mdt_extras.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def sort_by(queryset, order):
+    return queryset.order_by(order)
