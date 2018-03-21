37afa550985e01649269ef4c337a04e1fa3add84
theocole <theo.j.cole@gmail.com>
2018-03-21 16:38:48 +0000

    removed ad hoc queries from GEL IR model

--- gelweb/gel2mdt/api/serializers.py
+++ gelweb/gel2mdt/api/serializers.py
@@ -17,6 +17,41 @@ class ProbandSerializer(serializers.ModelSerializer):
         )
 
 
+class ProbandVariantSerializer(serializers.ModelSerializer):
+    variant = serializers.CharField(
+        source="variant.id",
+        read_only=True)
+    interpretation = serializers.CharField(
+        source="rare_disease_report.id",
+        read_only=True)
+    gene = serializers.CharField(
+        source="get_transcript.gene",
+        read_only=True)
+    transcript = serializers.CharField(
+        source="get_transcript.name",
+        read_only=True)
+    hgvs_c = serializers.CharField(
+        source="get_transcript_variant.hgvs_c",
+        read_only=True)
+    hgvs_p = serializers.CharField(
+        source="get_transcript_variant.hgvs_p",
+        read_only=True)
+
+
+    class Meta:
+        model = ProbandVariant
+        fields = (
+            "variant",
+            "interpretation",
+            "gene",
+            "transcript",
+            "hgvs_c",
+            "hgvs_p",
+            "zygosity",
+            "inheritance"
+        )
+
+
 class GELInterpretationReportSerializer(serializers.ModelSerializer):
 
     # fetch the IR family in a readable form
@@ -49,6 +84,16 @@ class GELInterpretationReportSerializer(serializers.ModelSerializer):
         read_only=True
     )
 
+    trio_sequenced = serializers.CharField(
+        source="ir_family.participant_family.trio_sequenced",
+        read_only=True
+    )
+
+    has_de_novo = serializers.BooleanField(
+        source="ir_family.participant_family.has_de_novo",
+        read_only=True
+    )
+
     case_type = serializers.BooleanField(
         source="ir_family.participant_family.proband.pilot_case",
         read_only=True
@@ -69,6 +114,8 @@ class GELInterpretationReportSerializer(serializers.ModelSerializer):
             'surname',
             'date_of_birth',
             'case_status',
+            'trio_sequenced',
+            'has_de_novo',
             'case_type',
             'ir_family',
             'archived_version',
