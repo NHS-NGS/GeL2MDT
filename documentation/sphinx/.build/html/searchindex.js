b4972d7fbbdd196301ab6af75cd1e8f008157591
theocole <theo.j.cole@gmail.com>
2018-04-06 11:30:27 +0100

    added sphinx doc folders and make

---
+++ documentation/sphinx/.build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index","modules/models"],envversion:53,filenames:["index.rst","modules/models.rst"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,model:0,modul:0,page:0,search:0},titles:["Welcome to GeL2MDT\u2019s documentation!","Models"],titleterms:{document:0,gel2mdt:0,indic:0,model:1,tabl:0,welcom:0}})
