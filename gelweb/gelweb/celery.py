b01caf64b415943b79c0180aad27ac88693bef10
Patrick Lombard <ptk.lmb55@gmail.com>
2018-04-19 10:40:54 +0100

    task docstrings; remove case filtering; removed config; sample in cip utils

--- gelweb/gelweb/celery.py
+++ gelweb/gelweb/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gelweb.settings.bateson_db')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gelweb.settings.charlie_db')
 
 app = Celery('gel2mdt')
 app.config_from_object('django.conf:settings', namespace='CELERY')
