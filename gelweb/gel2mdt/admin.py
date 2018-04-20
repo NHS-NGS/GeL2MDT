049420df207a51d183283ddff57c1c60c9feba21
Patrick Lombard <ptk.lmb55@gmail.com>
2018-04-20 11:08:50 +0100

    added revisions

--- gelweb/gel2mdt/admin.py
+++ gelweb/gel2mdt/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.apps import apps
-
+from reversion import revisions
 from gel2mdt.models import *
 
 
 app = apps.get_app_config('gel2mdt')
 for model_name, model in app.models.items():
     admin.site.register(model)
+    revisions.register(model)
+
