7b671f4bfe496f3a4021d695b0a0d5d13dd2847d
Patrick Lombard <ptk.lmb55@gmail.com>
2018-05-04 12:05:19 +0100

    Update DAILY_UPDATE.sh

--- gelweb/DAILY_UPDATE.sh
+++ gelweb/DAILY_UPDATE.sh
@@ -5,4 +5,4 @@ cd $HOME
 source bin/activate
 
 cd gel2mdt/gelweb
-python manage.py shell_plus < run_batch_update.py --settings=gelweb.settings.nginx_live
+python manage.py shell_plus < run_batch_update.py
