bde085c7ae66071170e07eb3ec093b924a1e9450
theocole <theo.j.cole@gmail.com>
2018-05-10 11:40:14 +0100

    conformed imports to PEP8

--- gelweb/gel2mdt/views.py
+++ gelweb/gel2mdt/views.py
@@ -19,7 +19,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import os, json, csv
+import os
+import json
+import csv
 from datetime import datetime
 from io import BytesIO, StringIO
 
