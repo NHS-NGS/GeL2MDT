2030d1b59bcd73e8be707de2da3adbc65249d1f1
Simon Boardman <simon.boardman@nhs.net>
2018-05-16 11:08:54 +0100

    Setup settings/base.py to import from local_settings.py

---
+++ gelweb/gelweb/settings/example_local_settings.py
@@ -0,0 +1,33 @@
+"""Local Settings for GeL2MDT"""
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Allowed hosts
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1']
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': '',
+        'NAME': 'gel2mdt_db',
+        'USER': '',
+        'PASSWORD': '',  # Need to set env var
+        'HOST': '',  # IP of the dbserver
+        'PORT': '',
+        'CONN_MAX_AGE': 500,
+        'OPTIONS': {
+            'sql_mode': 'TRADITIONAL',
+            'charset': 'utf8mb4',
+        },
+        'TEST': {
+            'NAME': 'django_test_gel2mdt',
+        },
+    },
+}
