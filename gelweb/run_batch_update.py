224e95267248a96ef96be0bfb0c5873f3ebcda0a
Ed Stone <stone_edward@hotmail.com>
2018-11-02 10:32:50 +0000

    modifications to report templates

--- gelweb/run_batch_update.py
+++ gelweb/run_batch_update.py
@@ -24,5 +24,5 @@ from gel2mdt.database_utils.multiple_case_adder import MultipleCaseAdder
 #mca = MultipleCaseAdder(sample_type='cancer', head=2, test_data=False, skip_demographics=True, pullt3=False)
 #mca.update_database()
 #del mca
-mca = MultipleCaseAdder(sample_type='raredisease', head=30, test_data=False, skip_demographics=True, pullt3=False)
-mca.update_database()
+mca = MultipleCaseAdder(sample_type='raredisease', head=None, test_data=False, skip_demographics=True, pullt3=False, bins=500)
+
