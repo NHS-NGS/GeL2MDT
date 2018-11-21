1fc3a6b69d5d2e012de7259e715de5ce59701671
Patrick Lombard <patrick.lombard@gosh.nhs.uk>
2018-11-21 11:13:08 +0000

    updated docker credentials

--- Docker/initial_script.sh
+++ Docker/initial_script.sh
@@ -3,8 +3,8 @@
 # This script only needs to be run once on first setting up the Gel2MDT database. Assuming a persistent db, any new containers will not require this to be run again
 
 # Create your own superuser account:
-DJANGO_SUPERUSER=lombap
-DJANGO_EMAIL=patrick.lombard@gosh.nhs.uk
+DJANGO_SUPERUSER=user
+DJANGO_EMAIL=user@center.nhs.uk
 DJANGO_PASSWORD=admin_password
 
 # Django creates the database schema
