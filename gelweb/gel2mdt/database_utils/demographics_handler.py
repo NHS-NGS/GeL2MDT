00fd4828c45f372e25c8b8885008829d2fbc4c1a
byronmews <patrick.lombard@gosh.nhs.uk>
2018-12-12 17:48:38 +0000

    saving up to this point

--- gelweb/gel2mdt/database_utils/demographics_handler.py
+++ gelweb/gel2mdt/database_utils/demographics_handler.py
@@ -18,63 +18,58 @@ class DemographicsHandler:
         self.server_context_ntgmc = lk.utils.create_server_context(
             'gmc.genomicsengland.nhs.uk', self.labkey_server_request_ntgmc, '/labkey', use_ssl=True)
 
-    def get_demographics_wl(self, participant_ids_wl):
+
+    def get_demographics(self, participant_ids):
+        server_context_list = [self.server_context_ntgmc, self.server_context_wlgmc]
+        all_results = []
+
         if self.sample_type == 'raredisease':
             schema_name = 'gel_rare_diseases'
         else:
             schema_name = 'gel_cancer'
+        
+        for participant_id in participant_ids:
+            query = participant_id
+            labkey_url_index = 0
+            all_gmc_labkeys_attempted = False
+            participant_demographics = {}
 
-        query = ';'.join(participant_ids_wl)
-        results = lk.query.select_rows(
-            server_context=self.server_context_wlgmc,
-            schema_name=schema_name,
-            query_name='participant_identifier',
-            filter_array=[
-                 lk.query.QueryFilter('participant_id', query, 'in')
-            ]
-        )
-        return results['rows']
+            while not all_gmc_labkeys_attempted:
+                # try most probable labkey url first
+                results = lk.query.select_rows(
+                    server_context=server_context_list[labkey_url_index],
+                    schema_name=schema_name,
+                    query_name='participant_identifier',
+                    filter_array=[
+                         lk.query.QueryFilter('participant_id', query, 'in')
+                    ]
+                )
+                try:
+                    participant_demographics["surname"] = results['rows'][0].get(
+                            'surname')
+                except IndexError as e:
+                    pass
 
-    def get_demographics_nt(self, participant_ids_nt):
-        if self.sample_type == 'raredisease':
-            schema_name = 'gel_rare_diseases'
-        else:
-            schema_name = 'gel_cancer'
+                if 'surname' in participant_demographics:
+                    all_results.append(results['rows'])
+                    all_gmc_labkeys_attempted = True # skip as other labkey url not required now
+                else:
+                    if labkey_url_index == 0:
+                        print("Participant", participant_id, "demographics not found in labkey path:", server_context_list[labkey_url_index]._container_path)
+                        labkey_url_index += 1
+                        print("Searching within alternate labkey path:", server_context_list[labkey_url_index]._container_path)
+                    else:
+                        print("Cannot find case demographics in labkey")
+                        all_gmc_labkeys_attempted = True
+                        pass
 
-        query = ';'.join(participant_ids_nt)
-        results = lk.query.select_rows(
-            server_context=self.server_context_ntgmc,
-            schema_name=schema_name,
-            query_name='participant_identifier',
-            filter_array=[
-                 lk.query.QueryFilter('participant_id', query, 'in')
-            ]
-        )
-        return results['rows']
-
-
-    def get_clinicians_wl(self, family_ids_wl):
-        if self.sample_type == 'raredisease':
-            schema_name = 'gel_rare_diseases'
-            query_name = 'rare_diseases_registration'
-            filter_type = 'family_id'
-        else:
-            schema_name = 'gel_cancer'
-            query_name = 'cancer_registration'
-            filter_type = 'participant_identifiers_id'
-        query = ';'.join(family_ids_wl)
-        results = lk.query.select_rows(
-            server_context=self.server_context_wlgmc,
-            schema_name=schema_name,
-            query_name=query_name,
-            filter_array=[
-                lk.query.QueryFilter(filter_type, query, 'in')
-            ]
-        )
-        return results['rows']
+        return all_results
 
 
-    def get_clinicians_nt(self, family_ids_nt):
+    def get_clinicians(self, family_ids):
+        server_context_list = [self.server_context_ntgmc, self.server_context_wlgmc]
+        all_results = []
+
         if self.sample_type == 'raredisease':
             schema_name = 'gel_rare_diseases'
             query_name = 'rare_diseases_registration'
@@ -83,41 +78,82 @@ class DemographicsHandler:
             schema_name = 'gel_cancer'
             query_name = 'cancer_registration'
             filter_type = 'participant_identifiers_id'
-        query = ';'.join(family_ids_nt)
-        results = lk.query.select_rows(
-            server_context=self.server_context_ntgmc,
-            schema_name=schema_name,
-            query_name=query_name,
-            filter_array=[
-                lk.query.QueryFilter(filter_type, query, 'in')
-            ]
-        )
-        return results['rows']      
 
-    def get_diagnosis_wl(self, participant_ids_wl):
-        # search in LabKey for recruited disease
-        if self.sample_type == 'raredisease':
-            query = ';'.join(participant_ids_wl)
-            results = lk.query.select_rows(
-                server_context=self.server_context_wlgmc,
-                schema_name='gel_rare_diseases',
-                query_name='rare_diseases_diagnosis',
-                filter_array=[
-                    lk.query.QueryFilter('participant_identifiers_id', query, 'in')
-                ]
-            )
-        return results['rows']
+        for family_id in family_ids:
+            query = family_id
+            labkey_url_index = 0
+            all_gmc_labkeys_attempted = False
+            clinician_details = {}
+            
+            while not all_gmc_labkeys_attempted:
+                # try most probable labkey url first
+                results = lk.query.select_rows(
+                    server_context=server_context_list[labkey_url_index],
+                    schema_name=schema_name,
+                    query_name=query_name,
+                    filter_array=[
+                        lk.query.QueryFilter(filter_type, query, 'contains')
+                    ]
+                )
+                try:
+                    clinician_details['name'] = results['rows'][0].get(
+                        'consultant_details_full_name_of_responsible_consultant')
+                except IndexError as e:
+                    pass
 
-    def get_diagnosis_nt(self, participant_ids_nt):
+                if 'name' in clinician_details:
+                    print("Found clinician details for family_id:", family_id)
+                    all_results.append(results['rows'])
+                    all_gmc_labkeys_attempted = True # skip as other labkey url not required now
+                else:
+                    if labkey_url_index == 0:
+                        print("Clinician not found in labkey path:", server_context_list[labkey_url_index]._container_path)
+                        labkey_url_index += 1
+                        print("Searching within alternate labkey path:", server_context_list[labkey_url_index]._container_path)
+                    else:
+                        print("Cannot find case clinician in labkey")
+                        all_gmc_labkeys_attempted = True
+                        pass
+        return all_results
+
+     
+    def get_diagnosis(self, participant_ids):
         # search in LabKey for recruited disease
+        server_context_list = [self.server_context_ntgmc, self.server_context_wlgmc]
+        all_results = []
+
         if self.sample_type == 'raredisease':
-            query = ';'.join(participant_ids_nt)
-            results = lk.query.select_rows(
-                server_context=self.server_context_ntgmc,
-                schema_name='gel_rare_diseases',
-                query_name='rare_diseases_diagnosis',
-                filter_array=[
-                    lk.query.QueryFilter('participant_identifiers_id', query, 'in')
-                ]
-            )
-        return results['rows']
+            for participant_id in participant_ids:
+                recruiting_disease = None
+                labkey_url_index = 0
+                all_gmc_labkeys_attempted = False
+
+                while not all_gmc_labkeys_attempted:
+                    query = participant_id
+                    results = lk.query.select_rows(
+                        server_context=server_context_list[labkey_url_index],
+                        schema_name='gel_rare_diseases',
+                        query_name='rare_diseases_diagnosis',
+                        filter_array=[
+                            lk.query.QueryFilter('participant_identifiers_id', query, 'in')
+                        ]
+                    )
+                try:
+                    recruiting_disease = results['rows'][0].get('gel_disease_information_specific_disease', None)
+                except IndexError as e:
+                    pass
+
+            if 'gel_disease_information_specific_disease' in recruiting_disease:
+                all_results.append(results['rows'])
+                all_gmc_labkeys_attempted = True # skip as other labkey url not required now
+            else:
+                if labkey_url_index == 0:
+                    print("Diagnosis not found in labkey path:", server_context_list[labkey_url_index]._container_path)
+                    labkey_url_index += 1
+                    print("Searching within alternate labkey path:", server_context_list[labkey_url_index]._container_path)
+                else:
+                    print("Cannot find case diagnosis in any labkey")
+                    all_gmc_labkeys_attempted = True
+                    pass
+        return all_results
+
